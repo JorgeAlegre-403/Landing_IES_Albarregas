efaf3e1e81b0ce6f6444d9d014827d96227c5068
yoryi404 <jalegrem04@iesalbarregas.es>
2026-04-13 19:23:24 +0200

    feat: creación de componente Carousel de Shadcn

---
+++ src/ui/carousel.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+import { cn } from "@/lib/utils"
+
+const Carousel = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("relative w-full", className)}
+    {...props}
+  />
+))
+Carousel.displayName = "Carousel"
+
+const CarouselContent = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("flex gap-4 overflow-x-auto snap-x snap-mandatory", className)}
+    {...props}
+  />
+))
+CarouselContent.displayName = "CarouselContent"
+
+const CarouselItem = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    ref={ref}
+    className={cn("snap-center shrink-0", className)}
+    {...props}
+  />
+))
+CarouselItem.displayName = "CarouselItem"
+
+export { Carousel, CarouselContent, CarouselItem }
