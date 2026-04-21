493183058e44cb6541521bc76c7ba52c188cde64
yoryi404 <jalegrem04@iesalbarregas.es>
2026-04-21 17:28:33 +0200

    refactor: mejorar navegación y accesibilidad del header

--- src/App.tsx
+++ src/App.tsx
@@ -6,9 +6,17 @@ import { ApartadoExtra } from './components/ApartadoExtra'
 import { Footer } from './components/Footer'
 
 function App() {
-
   return (
     <div className="w-full">
+      <a
+        href="#ciclos"
+        className="sr-only focus:not-sr-only focus:fixed focus:top-4 focus:left-4
+                   focus:z-[100] focus:px-4 focus:py-2 focus:bg-blue-600 focus:text-white
+                   focus:rounded-md focus:font-semibold focus:shadow-lg"
+      >
+        Saltar al contenido principal
+      </a>
+
       <Navbar />
       <Hero />
       <CiclosCarousel />
